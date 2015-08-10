9b9c6574cebf14f082bbc3beb3385e0093dcec92
Ben <benjamin.c.henderson@gmail.com>
2015-08-09 21:38:25 -0400

    write option select/unselect tests

--- tests/tests/optgroup-data.js
+++ tests/tests/optgroup-data.js
@@ -4,20 +4,6 @@ var Options = require('select2/options');
 var OptgroupData = require('optgroup-data');
 var selectOptions = new Options({});
 
-// test('current gets default for multiple', function (assert) {
-//   var $select = $('#qunit-fixture .multiple');
-//
-//   var data = new SelectData($select, selectOptions);
-//
-//   data.current(function (data) {
-//     assert.equal(
-//       data.length,
-//       0,
-//       'Multiple selects have no default selection.'
-//     );
-//   });
-// });
-
 // current
 test('returns option if they have :selected', function(assert){
     var $select = $('#qunit-fixture .multiple');
@@ -63,14 +49,55 @@ test('does not return option if it returns the optgroup', function(assert){
         assert.ok($select.find('optgroup:first').is(current[0].element));
     });
 });
-//
-// // select
-// test('selects the option', function(){});
-// test('if all sibling elements in optgroup are selected, selects the optgroup', function(){});
-//
-// // unselect
-// test('unselects the option', function(){});
-// test('unselects the parent optgroup', function(){});
+
+// select
+test('selects the option', function(assert){
+    var $select = $('#qunit-fixture .multiple');
+    var $option = $select.find('option:first');
+    var data = new OptgroupData($select, selectOptions);
+    var optionDatum = data.item($option);
+    assert.equal(optionDatum.element.selected, false);
+    data.select(optionDatum);
+    assert.ok($option.is(':selected'));
+});
+
+test('if all sibling elements in optgroup are selected, selects the optgroup', function(assert){
+    var $select = $('#qunit-fixture .multiple');
+    var $optgroup = $select.find('optgroup:first');
+    $optgroup.find('option').prop('selected', true);
+    var $firstOption = $optgroup.find('option:first').prop('selected', false);
+
+    var data = new OptgroupData($select, selectOptions);
+    assert.equal(optgroupIsSelected($optgroup), false);
+    data.select(  data.item($firstOption) );
+    assert.ok(optgroupIsSelected($optgroup));
+});
+
+
+// unselect
+test('unselects the option', function(assert){
+    var $select = $('#qunit-fixture .multiple');
+    var $option = $select.find('option:first').prop('selected', true);
+    var data = new OptgroupData($select, selectOptions);
+    var optionDatum = data.item($option);
+    assert.ok(optionDatum.element.selected);
+    data.unselect(optionDatum);
+    assert.equal($option.is(':selected'), false);
+});
+
+test('unselects the parent optgroup', function(assert){
+    var $select = $('#qunit-fixture .multiple');
+    var $optgroup = $select.find('optgroup:first');
+    selectOptgroup($optgroup);
+    $optgroup.find('option').prop('selected', true);
+    var $firstOption = $optgroup.find('option:first');
+
+    var data = new OptgroupData($select, selectOptions);
+    assert.equal(optgroupIsSelected($optgroup), true);
+    data.unselect(  data.item($firstOption) );
+    assert.equal(optgroupIsSelected($optgroup), false);
+});
+
 //
 // // optgroup select
 // test('selects the optgroup', function(){});
