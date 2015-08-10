5820960ebf673e0a0707c69174be67f4a7ee178a
Ben Henderson <ben@nestio.com>
2015-08-10 12:49:00 -0400

    write set classes tests

--- tests/tests/integration.js
+++ tests/tests/integration.js
@@ -47,7 +47,7 @@ test('Selecting all of the child elements selects the optgroup', function(assert
     assert.equal(title, $('#qunit-fixture .multiple optgroup:first').attr('label'));
 });
 
-test('Unselecting one of the child elements unselects the optgroup', function(assert){
+test('Unselecting one of the child elements in a fully selected optgroup shows the remaining options', function(assert){
     var $select = setUpSelect2($('#qunit-fixture .multiple'));
     $select.select2('open');
     $select.data('select2').$results.find('.select2-results__group:first').trigger('mouseup');
@@ -60,21 +60,58 @@ test('Unselecting one of the child elements unselects the optgroup', function(as
     assert.equal(title, $options.eq(1).text());
 });
 
+test('Clicking on choice remove link removes all options in the optgroup', function(assert){
+    var $select = setUpSelect2($('#qunit-fixture .multiple'));
+    $select.select2('open');
+    $select.data('select2').$results.find('.select2-results__group:first').trigger('mouseup');
+    $select.data('select2').$container.find('.select2-selection__choice__remove').click();
+    assert.notOk($select.val());
+});
+
 //setClasses
-// test('it sets all aria-selected[false] options in data to true', function(assert){});
-// test('it unsets all aria-selected[true] options not in the data', function(){});
-// test('it does not change all aria-selected[true] in the data', function(){});
-// test('it does not change all aria-selected[false] in the data', function(){});
+test('Clicking on optgroup sets it to aria-selected=true', function(assert){
+    var $select = setUpSelect2($('#qunit-fixture .multiple'));
+    $select.select2('open');
+    var $option = $select.data('select2').$results.find('.select2-results__group:first');
+    $option.trigger('mouseup');
+    assert.equal($option.attr('aria-selected'), 'true');
+});
 
-//
-// test('it sets all aria-selected[false] groups in data to true', function(){});
-// test('it unsets all aria-selected[true] groups not in the data', function(){});
-// test('it does not change all aria-selected[true] groups in the data', function(){});
-// test('it does not change all aria-selected[false] groups the data', function(){});
+test('Clicking on optgroup sets all the options to aria-selected=true', function(assert){
+    var $select = setUpSelect2($('#qunit-fixture .multiple'));
+    $select.select2('open');
+    var $option = $select.data('select2').$results.find('.select2-results__group:first');
+    $option.trigger('mouseup');
+    var $options = $select.data('select2').$results.find('.select2-results__option[aria-selected]');
+    assert.equal($options.eq(0).attr('aria-selected'), 'true');
+    assert.equal($options.eq(1).attr('aria-selected'), 'true');
+});
 
-//bind
-// test('mouseup on selected optgroup triggers optgroup:unselect and calls setClasses', function(){});
-// test('mouseup on unselected optgroup triggers optgroup:select and calls setClasses', function(){});
+test('Clicking on selected optgroup sets it to aria-selected=false', function(assert){
+    var $select = setUpSelect2($('#qunit-fixture .multiple'));
+    $select.select2('open');
+    var $option = $select.data('select2').$results.find('.select2-results__group:first');
+    $option.trigger('mouseup');
+    $select.select2('open');
+    assert.equal($option.attr('aria-selected'), 'true');
+    $option = $select.data('select2').$results.find('.select2-results__group:first');
+    $option.trigger('mouseup');
+    assert.equal($option.attr('aria-selected'), 'false');
+});
+
+test('Clicking on selected optgroup sets all of the options to aria-selected=false', function(assert){
+    var $select = setUpSelect2($('#qunit-fixture .multiple'));
+    $select.select2('open');
+    $select.data('select2').$results.find('.select2-results__group:first').trigger('mouseup');
+    $select.select2('open');
+    $select.data('select2').$results.find('.select2-results__group:first').trigger('mouseup');
+    var $options = $select.data('select2').$results.find('.select2-results__option[aria-selected]');
+    assert.equal($options.eq(0).attr('aria-selected'), 'false');
+    assert.equal($options.eq(1).attr('aria-selected'), 'false');
+});
+
+//closeOnSelect
+
 // test('mouseup on unselected optgroup closes container if closeOnSelect=true', function(){});
 // test('mouseup on unselected optgroup keeps container open if closeOnSelect=false', function(){});
 
