017451ec1140b94f4fc4f0de0b21d44798c9d2d1
Ben Henderson <ben@nestio.com>
2015-08-10 12:30:10 -0400

    begin writing integration specs

--- tests/tests/integration.js
+++ tests/tests/integration.js
@@ -1,12 +1,64 @@
 module('Integration');
 
+// changing the val
 test('Selecting an unselected optgroup selects all the child elements', function(assert){
-    var $select = $('#qunit-fixture .multiple');
+    var $select = setUpSelect2($('#qunit-fixture .multiple'));
+    $select.select2('open');
+    $select.data('select2').$results.find('.select2-results__group:first').trigger('mouseup');
+    assert.deepEqual(["one", "two"], $select.val());
 });
 
-// test('Selecting a selected optgroup selects all the child elements', function(){});
-// test('Selecting all of the child elements selects the optgroup', function(){});
-// test('Unselecting one of the child elements unselects the optgroup', function(){});
+test('Selecting a partially selected optgroup selects all the child elements', function(assert){
+    var $select = setUpSelect2($('#qunit-fixture .multiple'));
+    $select.val(["one"]);
+    $select.select2('open');
+    $select.data('select2').$results.find('.select2-results__group:first').trigger('mouseup');
+    assert.deepEqual(["one", "two"], $select.val());
+});
+
+test('Clicking on a selected optgroup unselects all of the options', function(assert){
+    var $select = setUpSelect2($('#qunit-fixture .multiple'));
+    $select.select2('open');
+    $select.data('select2').$results.find('.select2-results__group:first').trigger('mouseup');
+    assert.deepEqual(["one", "two"], $select.val());
+    $select.select2('open');
+    $select.data('select2').$results.find('.select2-results__group:first').trigger('mouseup');
+    assert.notOk($select.val());
+});
+
+// selection
+
+test('Clicking on the optgroup causes the optgroup to display in the selection', function(assert){
+    var $select = setUpSelect2($('#qunit-fixture .multiple'));
+    $select.select2('open');
+    $select.data('select2').$results.find('.select2-results__group:first').trigger('mouseup');
+    var title = $select.data('select2').$container.find('.select2-selection__choice').attr('title');
+    assert.equal(title, $('#qunit-fixture .multiple optgroup:first').attr('label'));
+});
+
+test('Selecting all of the child elements selects the optgroup', function(assert){
+    var $select = setUpSelect2($('#qunit-fixture .multiple'));
+    $select.select2('open');
+    var $options = $select.data('select2').$results.find('.select2-results__option[aria-selected]');
+    $options.eq(0).trigger('mouseup');
+    $options.eq(1).trigger('mouseup');
+    
+    var title = $select.data('select2').$container.find('.select2-selection__choice').attr('title');
+    assert.equal(title, $('#qunit-fixture .multiple optgroup:first').attr('label'));
+});
+
+test('Unselecting one of the child elements unselects the optgroup', function(assert){
+    var $select = setUpSelect2($('#qunit-fixture .multiple'));
+    $select.select2('open');
+    $select.data('select2').$results.find('.select2-results__group:first').trigger('mouseup');
+    
+    $select.select2('open');
+    var $options = $select.data('select2').$results.find('.select2-results__option[aria-selected]');
+    $options.eq(0).trigger('mouseup');
+    
+    var title = $select.data('select2').$container.find('.select2-selection__choice').attr('title');
+    assert.equal(title, $options.eq(1).text());
+});
 
 //setClasses
 // test('it sets all aria-selected[false] options in data to true', function(assert){});
@@ -25,9 +77,4 @@ test('Selecting an unselected optgroup selects all the child elements', function
 // test('mouseup on unselected optgroup triggers optgroup:select and calls setClasses', function(){});
 // test('mouseup on unselected optgroup closes container if closeOnSelect=true', function(){});
 // test('mouseup on unselected optgroup keeps container open if closeOnSelect=false', function(){});
-
-//option
-//var $label = $(option).find('.select2-results__group');
-// test('gives the group role=treeitem, aria-selected=false', function(){})
-// test('gives the group.data', function(){})
 
