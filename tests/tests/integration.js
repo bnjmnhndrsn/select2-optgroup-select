43a28bedeb8d414ac14bd3ce28ff4f4c81ead10f
Ben Henderson <ben@nestio.com>
2015-08-10 11:26:44 -0400

    add in sinon

--- tests/tests/integration.js
+++ tests/tests/integration.js
@@ -1,6 +1,33 @@
 module('Integration');
 
-// test('Selecting an unselected optgroup selects all the child elements', function(){});
+test('Selecting an unselected optgroup selects all the child elements', function(assert){
+    var $select = $('#qunit-fixture .multiple');
+});
+
 // test('Selecting a selected optgroup selects all the child elements', function(){});
 // test('Selecting all of the child elements selects the optgroup', function(){});
 // test('Unselecting one of the child elements unselects the optgroup', function(){});
+
+//setClasses
+// test('it sets all aria-selected[false] options in data to true', function(assert){});
+// test('it unsets all aria-selected[true] options not in the data', function(){});
+// test('it does not change all aria-selected[true] in the data', function(){});
+// test('it does not change all aria-selected[false] in the data', function(){});
+
+//
+// test('it sets all aria-selected[false] groups in data to true', function(){});
+// test('it unsets all aria-selected[true] groups not in the data', function(){});
+// test('it does not change all aria-selected[true] groups in the data', function(){});
+// test('it does not change all aria-selected[false] groups the data', function(){});
+
+//bind
+// test('mouseup on selected optgroup triggers optgroup:unselect and calls setClasses', function(){});
+// test('mouseup on unselected optgroup triggers optgroup:select and calls setClasses', function(){});
+// test('mouseup on unselected optgroup closes container if closeOnSelect=true', function(){});
+// test('mouseup on unselected optgroup keeps container open if closeOnSelect=false', function(){});
+
+//option
+//var $label = $(option).find('.select2-results__group');
+// test('gives the group role=treeitem, aria-selected=false', function(){})
+// test('gives the group.data', function(){})
+
